7b226a972fc5ca34d08b98d5d53d667ee335a3c2
Aayush <aayush2958@gmail.com>
2025-07-02 10:42:59 +0000

    Small CHange

---
+++ my-gcp-terraform/main.tf
@@ -0,0 +1,25 @@
+provider "google" {
+  credentials = file(var.credentials_file)
+  project     = var.project_id
+  region      = var.region
+  zone        = var.zone
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "my-first-instance"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+
+  network_interface {
+    network       = "default"
+    access_config {}
+  }
+
+  tags = ["web", "dev"]
+}
